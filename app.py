4fdde654b8fd809dd78b89ca138bea59b11524a9
Water Ibarrola <walter_ibarrola@hotmail.com.ar>
2025-09-06 08:33:34 -0300

    - Se agrega lógica de formulario

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html", materia="PPS", lista=["Walter", "Ana", "Damian", "Pablo", "Angel", "Catalina", "Valentin", "Gustavo"])
+
+@app.route("/formulario", methods=["GET"])
+def home_get():
+    return render_template("formulario.html")
+
+@app.route("/formulario", methods=["POST"])
+def home_post():
+    nombre = request.form.get("nombre")
+    email = request.form.get("email")
+    return render_template("resultado.html", nombre=nombre, email=email)
 
 if __name__ == "__main__":
     app.run(debug=True)
