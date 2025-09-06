8befad88d98937b70b5b4027820f199951d7c912
Walter Ibarrola <walter_ibarrola@hotmail.com.ar>
2025-09-06 13:36:59 -0300

    Se agrega clases dueño y clase mascota al app.py

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Clases
+
+class Dueno:
+    def __init__(self, id, nombre, telefono):
+        self.id = id
+        self.nombre = nombre
+        self.mascotas = []  # lista de ids de mascotas?
+
+class Mascota:
+    def __init__(self, id, nombre, especie, edad, dueno_id):
+        self.id = id
+        self.nombre = nombre
+        self.especie = especie
+        self.dueno_id = dueno_id
+
+# Rutas
 @app.route("/")
 def home():
     return render_template("index.html", materia="PPS", lista=["Walter", "Ana", "Damian", "Pablo", "Angel", "Catalina", "Valentin", "Gustavo"])
