2e2056e25047e29258c8b008b8015a7b3563ee93
Water Ibarrola <walter_ibarrola@hotmail.com.ar>
2025-09-06 14:42:04 -0300

    - Se elimina formulario de prueba
    - Se agrega y modulariza el código en arquitectura MVC

--- app.py
+++ app.py
@@ -1,36 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask
+from controllers.home_controller import home_bp
 
 app = Flask(__name__)
 
-# Clases
-
-class Dueno:
-    def __init__(self, id, nombre, telefono):
-        self.id = id
-        self.nombre = nombre
-        self.mascotas = []  # lista de ids de mascotas?
-
-class Mascota:
-    def __init__(self, id, nombre, especie, edad, dueno_id):
-        self.id = id
-        self.nombre = nombre
-        self.especie = especie
-        self.dueno_id = dueno_id
-
-# Rutas
-@app.route("/")
-def home():
-    return render_template("index.html", materia="PPS", lista=["Walter", "Ana", "Damian", "Pablo", "Angel", "Catalina", "Valentin", "Gustavo"])
-
-@app.route("/formulario", methods=["GET"])
-def home_get():
-    return render_template("formulario.html")
-
-@app.route("/formulario", methods=["POST"])
-def home_post():
-    nombre = request.form.get("nombre")
-    email = request.form.get("email")
-    return render_template("resultado.html", nombre=nombre, email=email)
+# Registrar los Blueprint de los controladores
+app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ models/mascota.py
@@ -0,0 +1,6 @@
+class Mascota:
+    def __init__(self, id, nombre, especie, dueno_id): # Agregar atributos
+        self.id = id
+        self.nombre = nombre
+        self.especie = especie
+        self.dueno_id = dueno_id
