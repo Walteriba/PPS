9ee5740f24a6b2b8545bed304a32c137495c1f9d
Water Ibarrola <walter_ibarrola@hotmail.com.ar>
2025-09-18 14:59:42 -0300

    - Se agrega lógica de DTO para pacientes en home (#16)

---
+++ dto/paciente_dto.py
@@ -0,0 +1,7 @@
+class PacienteDTO:
+    def __init__(self, id, nombre, especie, raza, tutor):
+        self.id = id
+        self.nombre = nombre
+        self.especie = especie
+        self.raza = raza
+        self.tutor = tutor
