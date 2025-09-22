9ae4b626194a9cf8cbd463b73e98593a08a1c6cb
damian <damianlibovich@gmail.com>
2025-09-22 17:47:14 -0300

    actualizacion_prueba

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask
 from models import db
 from controllers.home_controller import home_bp
 from utils.cloudinary_utils import subir_y_obtener_url
-<<<<<<< HEAD
-from flask import request
-
-=======
 from flask import request, redirect, url_for, render_template, flash
 from controllers.paciente_controller import paciente_bp
->>>>>>> 265ce1e73fb65105f2082e9f2b0e822ee8831338
+
 
 
 app = Flask(__name__)
