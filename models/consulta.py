96e0b131d43628bb5d2fd865b6ca12f032da2bcd
Water Ibarrola <walter_ibarrola@hotmail.com.ar>
2025-09-30 20:21:22 -0300

    - Se elimina tutor_id

--- models/consulta.py
+++ models/consulta.py
@@ -7,7 +7,7 @@ class Consulta(db.Model):
     __tablename__ = "consultas"
 
     id = db.Column(db.Integer, primary_key=True)
-    tutor_id = db.Column(db.Integer, db.ForeignKey("tutores.id"), nullable=False)
+    tutor_id = db.Column(db.Integer, db.ForeignKey("tutores.id"), nullable=False) # TODO: Borrar
     paciente_id = db.Column(db.Integer, db.ForeignKey("pacientes.id"), nullable=False)
     fecha = db.Column(db.Date, default=date.today)
     peso = db.Column(db.Float, nullable=False)
