e802c38a9daa829f55c7f0985c3f17d57bd25b77
damian <damianlibovich@gmail.com>
2025-10-08 14:06:22 -0300

    actualizacion

---
+++ models/archivos.py
@@ -0,0 +1,14 @@
+from models import db
+
+class Archivo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255), nullable=False)
+    paciente_id = db.Column(db.Integer, db.ForeignKey('pacientes.id'), nullable=False)
+    consulta_id = db.Column(db.Integer, db.ForeignKey('consultas.id'), nullable=True)
+    
+    paciente = db.relationship('Paciente', backref=db.backref('archivos', lazy=True))
+    consulta = db.relationship('Consulta', backref=db.backref('archivos', lazy=True))
+
+
+    def __repr__(self):
+        return f'<Archivo {self.id} - {self.url}>'
