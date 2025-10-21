8221449618290f4ccca5fc2c94f78d8368d2f8e7
Mozotegui Catalina <mozotegui.cat@gmail.com>
2025-10-21 14:31:53 -0300

    Resuelto conflicto de fusión con main

--- controllers/consulta_controller.py
+++ controllers/consulta_controller.py
@@ -13,16 +13,8 @@ from models.archivo import Archivo
 
 consulta_bp = Blueprint("consulta_bp", __name__)  # Definición del Blueprint
 
-<<<<<<< HEAD
-consulta_bp = Blueprint("consulta_bp", __name__)     
-
-# Endpoint para crear consulta 
-@consulta_bp.route("/consulta/nuevo", methods=["GET", "POST"])
-=======
-
 # Endpoint para crear consulta (insert)
 @consulta_bp.route("/consulta/nuevo", methods=["POST"])
->>>>>>> origin/main
 def crear_consulta():
     """Crear una nueva consulta médica."""
     
@@ -40,27 +32,18 @@ def crear_consulta():
         return jsonify({"error": f"Campo obligatorio faltante: {e}"}), 400
 
     nueva_consulta = Consulta(
-<<<<<<< HEAD
         fecha=fecha_consulta,
         
         # Campos de texto sin límite de caracteres
-=======
-        fecha=datetime.strptime(request.form["fecha"], "%Y-%m-%d"),
         peso=float(request.form["peso"]),  # Validar, no puede ser más de 100kg
         temperatura=float(request.form["temperatura"]),  # Validar, no puede ser más de 50 grados
->>>>>>> origin/main
         anamnesis=request.form.get("anamnesis"),
         examen_fisico=request.form.get("examen_fisico"),
         diagnostico=request.form.get("diagnostico"),
         tratamiento=request.form.get("tratamiento"),
-<<<<<<< HEAD
         
         tutor_id=tutor_id_consulta,
         paciente_id=paciente_id_consulta
-=======
-        tutor_id=int(request.form["tutor_id"]),
-        paciente_id=int(request.form["paciente_id"]),
->>>>>>> origin/main
     )
     
     db.session.add(nueva_consulta)
@@ -78,7 +61,6 @@ def crear_consulta():
         jsonify({"mensaje": "Consulta creada con éxito", "id": nueva_consulta.id}),
         201,
     )
-
 
 # --------------------------------------------------------------------------------------
 
@@ -104,7 +86,6 @@ def actualizar_consulta(id_consulta):
     if anamnesis is not None:
         consulta.anamnesis = anamnesis
 
-<<<<<<< HEAD
     # 3. Examen Físico 
     examen_fisico = request.form.get("examen_fisico")
     if examen_fisico is not None:
@@ -120,8 +101,7 @@ def actualizar_consulta(id_consulta):
     if tratamiento is not None:
         consulta.tratamiento = tratamiento
         
-    # Guardar cambios en la base de datos
-=======
+
     # Lógica para AGREGAR nuevos archivos
     archivos = request.files.getlist("archivos")
     for archivo in archivos:
@@ -130,159 +110,14 @@ def actualizar_consulta(id_consulta):
             nuevo_archivo = Archivo(url=url)
             consulta.archivos.append(nuevo_archivo)
 
->>>>>>> origin/main
+# Guardar cambios en la base de datos
     db.session.commit()
 
     return jsonify(
         {"mensaje": "Consulta actualizada exitosamente", "consulta_id": id_consulta}
     )
 
-<<<<<<< HEAD
-# --------------------------------------------------------------------------------------
-
-# Endpoint para eliminar una consulta
-@consulta_bp.route("/consulta/<int:id_consulta>", methods=["DELETE"])
-def eliminar_consulta(id_consulta):
-    # Buscar consulta por ID
-    consulta = Consulta.query.get(id_consulta)
-    if not consulta:
-        return jsonify({"error": "Consulta no encontrada"}), 404
-    # Eliminar de la base de datos
-    db.session.delete(consulta)
-    db.session.commit()
-    return jsonify({"mensaje": "Consulta eliminada con éxito", "id": id_consulta}), 200
-=======
->>>>>>> origin/main
-
-# --------------------------------------------------------------------------------------
-
-# Endpoint para obtener todas las consultas
-@consulta_bp.route("/consultas", methods=["GET"])
-def obtener_consultas():
-    consultas = Consulta.query.all()
-    resultado = []
-    for consulta in consultas:
-<<<<<<< HEAD
-        resultado.append({
-            "id": consulta.id,
-            "fecha": consulta.fecha.strftime("%Y-%m-%d"),
-            "anamnesis": consulta.anamnesis,
-            "examen_fisico": consulta.examen_fisico,
-            "diagnostico": consulta.diagnostico,
-            "tratamiento": consulta.tratamiento,
-            "tutor_id": consulta.tutor_id,
-            "paciente_id": consulta.paciente_id
-        })
-    return jsonify(resultado), 200
-
-# --------------------------------------------------------------------------------------
-=======
-        resultado.append(
-            {
-                "id": consulta.id,
-                "fecha": consulta.fecha.strftime("%Y-%m-%d"),
-                "peso": consulta.peso,
-                "temperatura": consulta.temperatura,
-                "anamnesis": consulta.anamnesis,
-                "examen_fisico": consulta.examen_fisico,
-                "diagnostico": consulta.diagnostico,
-                "tratamiento": consulta.tratamiento,
-                "tutor_id": consulta.tutor_id,
-                "paciente_id": consulta.paciente_id,
-                "archivo": [archivo.url for archivo in consulta.archivos],
-            }
-        )
-    return jsonify(resultado), 200
-
->>>>>>> origin/main
-
-# Endpoint para obtener una consulta por paciente
-@consulta_bp.route("/consultas/paciente/<int:paciente_id>", methods=["GET"])
-def obtener_consultas_por_paciente(paciente_id):
-    consultas = Consulta.query.filter_by(paciente_id=paciente_id).all()
-    if not consultas:
-        return jsonify({"error": "No se encontraron consultas para este paciente"}), 404
-    resultado = []
-    for consulta in consultas:
-<<<<<<< HEAD
-        resultado.append({
-            "id": consulta.id,
-            "fecha": consulta.fecha.strftime("%Y-%m-%d"),
-            "anamnesis": consulta.anamnesis,
-            "examen_fisico": consulta.examen_fisico,
-            "diagnostico": consulta.diagnostico,
-            "tratamiento": consulta.tratamiento,
-            "tutor_id": consulta.tutor_id,
-            "paciente_id": consulta.paciente_id
-        })
-    return jsonify(resultado), 200
-
-# --------------------------------------------------------------------------------------
-=======
-        resultado.append(
-            {
-                "id": consulta.id,
-                "fecha": consulta.fecha.strftime("%Y-%m-%d"),
-                "peso": consulta.peso,
-                "temperatura": consulta.temperatura,
-                "anamnesis": consulta.anamnesis,
-                "examen_fisico": consulta.examen_fisico,
-                "diagnostico": consulta.diagnostico,
-                "tratamiento": consulta.tratamiento,
-                "tutor_id": consulta.tutor_id,
-                "paciente_id": consulta.paciente_id,
-                "archivo": [archivo.url for archivo in consulta.archivos],
-            }
-        )
-    return jsonify(resultado), 200
-
->>>>>>> origin/main
-
-# Endpoint para obtener una consulta por anamnesis
-@consulta_bp.route("/consultas/anamnesis/<string:anamnesis>", methods=["GET"])
-def obtener_consultas_por_anamnesis(anamnesis):
-    consultas = Consulta.query.filter(Consulta.anamnesis.contains(anamnesis)).all()
-    if not consultas:
-        return jsonify({"error": "No se encontraron consultas con esta anamnesis"}), 404
-    resultado = []
-    for consulta in consultas:
-<<<<<<< HEAD
-        resultado.append({
-            "id": consulta.id,
-            "fecha": consulta.fecha.strftime("%Y-%m-%d"),
-            "anamnesis": consulta.anamnesis,
-            "examen_fisico": consulta.examen_fisico,
-            "diagnostico": consulta.diagnostico,
-            "tratamiento": consulta.tratamiento,
-            "tutor_id": consulta.tutor_id,
-            "paciente_id": consulta.paciente_id
-        })
-    return jsonify(resultado), 200
-
-# --------------------------------------------------------------------------------------
-
-# Endpoint para ver una consulta específica (consulta.html existe ????)
-=======
-        resultado.append(
-            {
-                "id": consulta.id,
-                "fecha": consulta.fecha.strftime("%Y-%m-%d"),
-                "peso": consulta.peso,
-                "temperatura": consulta.temperatura,
-                "anamnesis": consulta.anamnesis,
-                "examen_fisico": consulta.examen_fisico,
-                "diagnostico": consulta.diagnostico,
-                "tratamiento": consulta.tratamiento,
-                "tutor_id": consulta.tutor_id,
-                "paciente_id": consulta.paciente_id,
-                "archivo": [archivo.url for archivo in consulta.archivos],
-            }
-        )
-    return jsonify(resultado), 200
-
-
 # Endpoint para ver una consulta específica
->>>>>>> origin/main
 @consulta_bp.route("/consulta/<int:consulta_id>", methods=["GET"])
 def ver_consulta(consulta_id):
     consulta = Consulta.query.get_or_404(consulta_id)
