90dc8113ee4f364f1c214cab4f23309bf523b593
Valenting04 <valentingarciaa04@gmail.com>
2025-10-25 15:36:11 -0300

    Vista de login y register

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from controllers.home_controller import home_bp
 from controllers.paciente_controller import paciente_bp
 from controllers.consulta_controller import consulta_bp
 from controllers.tutor_controller import tutor_bp
+from controllers.auth_controller import auth_bp
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///vetlog.db"
@@ -20,6 +21,7 @@ app.register_blueprint(home_bp)
 app.register_blueprint(paciente_bp)
 app.register_blueprint(consulta_bp)
 app.register_blueprint(tutor_bp)
+app.register_blueprint(auth_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
