c9d3ee2ab82fcb6af058062b051766182b332cd1
Water Ibarrola <walter_ibarrola@hotmail.com.ar>
2025-11-21 13:44:23 -0300

    fix rebase

--- controllers/tutor_controller.py
+++ controllers/tutor_controller.py
@@ -17,14 +17,9 @@ def ver_nuevo_tutor():
 @tutor_bp.route("/tutor/<int:id>/editar", methods=["GET"])
 @login_required
 def ver_actualizar_tutor(id):
-<<<<<<< HEAD
     paciente_id = request.args.get("paciente_id", type=int)
-    tutor = Tutor.query.get_or_404(id)
-    paciente = Paciente.query.get_or_404(paciente_id)
-=======
     tutor = Tutor.query.filter_by(id=id, user_id=current_user.id).first_or_404()
-    paciente = Paciente.query.filter_by(tutor_id=tutor.id).first()
->>>>>>> e7c92cd (modificaciones para probar transformar la app en multiusuarios)
+    paciente = Paciente.query.filter_by(id=paciente_id, tutor_id=tutor.id).first_or_404()
     return render_template("tutor/editar_tutor.html", tutor=tutor, paciente=paciente)
 
 
